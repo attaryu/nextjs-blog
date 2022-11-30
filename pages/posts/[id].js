ce1e099687c9fbb22009f666e1983b780016333a
attaryu <mattarannaufal@gmail.com>
2022-12-01 03:06:57 +0700

    fixing directory

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Layout from '../../components/layout'
+import Layout from '../../components/Layout'
 import Date from '../../components/date';
 import { getAllPostIds, getPostData } from '../../utils/posts'
 import utilStyles from '../../styles/utils.module.css';
